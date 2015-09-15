e09879d07a0ded07921e666772948d767cff4a64
Melissa Leigh Gore <melissaleighgore@gmail.com>
2015-09-15 13:03:21 -0400

    Remove API stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
 
-  namespace :api do
-    namespace :v1 do
-      resources :poems, only: [:index, :show]
-      get '/poems/all/random', to: 'poems#random'
-
-      resources :lines, only: [:index, :show]
-      get '/lines/all/random', to: 'lines#random'
-    end
-  end
-
   resources :poems, only: [:index, :show]
 
 end
