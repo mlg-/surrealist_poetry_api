50c4aed9ad140202ee45c65409f3abcb8cdbdb3c
Melissa Leigh Gore <melissaleighgore@gmail.com>
2015-09-15 20:54:15 -0400

    Add error handling example to Line#show

--- app/controllers/api/v1/lines_controller.rb
+++ app/controllers/api/v1/lines_controller.rb
@@ -7,6 +7,13 @@ module API
         render json: @lines
       end
 
+      def show
+        line = Line.find(params[:id])
+        render json: line
+      rescue ActiveRecord::RecordNotFound
+        return render json: { error: 'Line not found' }, status: 404
+      end
+
       def random 
         @line = Line.all.sample
         render json: @line
